ee0c62a6f1ebbde98c4630d2bc2e78c2b22fc74c
Mark Rigden <mr632@kent.ac.uk>
2022-01-17 14:34:04 +0000

    Added model to run python scripts

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\PythonModel;
 
-class HomeController extends Controller
-{
-    public function index(){
+class HomeController extends Controller {
+    public function index() {
+        print_r(PythonModel::runHerokuTest("Hello World", 10, "False", "None"));
+        
         return view('home');
     }
 }
