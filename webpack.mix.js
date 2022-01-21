741268a9dda400eb14b11d55cc0f194ce9381f6b
vitaly <viv55941@icloud.com>
2022-01-21 13:17:01 +0000

    browserSync added

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,9 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .vue()
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .postCss('resources/css/app.css', 'public/css');
+mix.browserSync({
+    // proxy: 'http://127.0.0.1:8000',
+    // browser: 'google chrome',
+});
