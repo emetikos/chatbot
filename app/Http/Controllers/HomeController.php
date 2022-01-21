81f095b4db065e96f5d0873b158e90a350d1331b
vitaly <viv55941@icloud.com>
2022-01-21 15:06:10 +0000

    fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,9 +7,7 @@ use App\Models\PythonModel;
 class HomeController extends Controller {
     public function index() {
         print_r(PythonModel::runHerokuTest("Hello World", 10, "False", "None"));
-        
         print_r(PythonModel::runChatbot("Hello World", 10, "False", "None"));
-        
         return view('home');
     }
 }
