e896925c62aea82b5433524de1af1daf15941f6f
Jake Woodcock <jw901@kent.ac.uk>
2022-01-24 15:09:31 +0000

    tested functions, successfully store to and retrieve from database

--- app/Http/Controllers/TableController.php
+++ app/Http/Controllers/TableController.php
@@ -5,25 +5,18 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-class HomeController extends Controller
+class TableController extends Controller
 {
-    public function retrieve_same($in){
-        $samequery = DB::select('SELECT DISTINCT path_name FROM resources r JOIN queries q ON r.query_id=q.id WHERE q.name = ?', [$in]);
+    public function retrieve_same(){
+        $in = '1';
+        $samequery = DB::select('SELECT path_name FROM resources r JOIN queries q ON r.query_id=q.id WHERE q.name = ?', [$in]);
+        dd($samequery);
     }
 
-    public function store($query, $link){
-        $storequery = DB::table('query')
-            ->insert([
-                'name' => $query
-            ]);
-        $getquery = DB::table('query')
-            //->select('id')
-            //->where('name', $query)
-            ->last();
-        $storelink = DB::table('resources')
-            ->insert([
-                'query_id' => $getquery,
-                'path_name' => $link
-            ]);
+    public function store(){
+        $query = '1';
+        $link = 'asdf';
+        $storequery = DB::insert('INSERT INTO queries (name) VALUES (?)', [$query]);
+        $storequery = DB::insert('INSERT INTO resources (query_id, path_name) VALUES ((SELECT MAX(id) FROM queries), ?)', [$link]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TableController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,10 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
 
+//Route::get('/store', [TableController::class, 'store']);
+//Route::get('/same', [TableController::class, 'retrieve_same']);
 
 //Route::get('/', function () {
 //    return view('welcome');
 //});
-
-
-
