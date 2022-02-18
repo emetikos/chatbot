54dd4911ed2f6d4c3d5f4a9a0d3ab79110f736c5
emetikos <spirosvelos@yahoo.com>
2022-02-18 09:30:27 +0000

    components for fetching and displaying data

---
+++ resources/js/components/TopicItem.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="topic-item">
+
+        <button @click="$emit('print-topic', topic)" type="submit">{{ topic }}</button>
+
+</div>
+
+</template>
+
+<script>
+    export default {
+        name: 'topicItem',
+        props:['topic']
+    }
+</script>
+
+<style scoped>
+
+.topic-item {
+    margin: .3rem;
+    width: 100%;
+    padding:.5rem  .5rem;
+
+}
+</style>
---
+++ resources/js/components/Topics.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <div class="topics-not-found">
+            <h4>This is what I found</h4>
+            <div class="topics" >
+                <ul>
+                    <li v-for="topic in topics">
+                        <TopicItem v-on:print-topic="$emit('print-topic', topic)" v-bind:topic="topic"/>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+import TopicItem from './TopicItem';
+export default {
+    name:"topics",
+    components:{
+      TopicItem
+    },
+    props:['topics']
+}
+
+</script>
+
+<style scoped>
+
+.topics-found {
+    display: block;
+}
+
+.topics-not-found {
+    display: none;
+}
+.topics {
+    max-width: 12rem;
+    border:1px solid black;
+    border-radius: 4px;
+    /*padding: .5rem;*/
+}
+
+
+
+</style>
