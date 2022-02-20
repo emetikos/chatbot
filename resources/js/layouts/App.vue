b214125ec1da210185d112b6f303c7a3bbd52ef4
vitaly <vi30@kent.ac.uk>
2022-02-20 19:28:46 +0000

    additional changes for the main chatbot interface template

--- resources/js/layouts/App.vue
+++ resources/js/layouts/App.vue
@@ -1,18 +1,28 @@
 <template>
     <section class="chat-bot">
         <div class="chat-bot__container">
+            <div class="chat-bot__header">
 
-<!--      <example-component/>-->
+            </div>
+            <div class="chat-bot__main">
+                <!--<example-component/>-->
+                <main-window-component/>
+            </div>
+            <div class="chat-bot__footer">
+
+            </div>
         </div>
     </section>
 </template>
 
 <script>
 // import ExampleComponent from '../components/ExampleComponent';
+import MainWindowComponent from '../components/MainWindowComponent';
 
 export default {
   components: {
     // ExampleComponent
+      MainWindowComponent
   }
 }
 </script>
