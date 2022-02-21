e28bac8f3854c552f809f84e6e02a5701a70f583
emetikos <spirosvelos@yahoo.com>
2022-02-21 09:38:29 +0000

    change the component functionality to be in the component and not in the wrapper

--- resources/js/layouts/App.vue
+++ resources/js/layouts/App.vue
@@ -1,22 +1,16 @@
 <template>
   <div class="container">
-      <example-component/>
       <Topics/>
-      <Logo/>
 
   </div>
 </template>
 
 <script>
-import ExampleComponent from '../components/ExampleComponent';
 import Topics from '../components/Topics';
-import Logo from '../components/Logo';
 
 export default {
   components: {
-    ExampleComponent,
     Topics,
-    Logo,
   }
 }
 </script>
