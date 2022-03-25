b55370d2c40d14c5e409bdae209a72a0cfbde341
vitaly <vi30@kent.ac.uk>
2022-03-25 16:09:32 +0000

    comments added for the api function in the home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -18,36 +18,69 @@ class HomeController extends Controller {
     }
 
     /**
+     * Main function for a request from a user.
+     * Sends and receive data to and from the chatbot
+     * Accepts the user input values and store required flags in a session to support communication
+     *
      * @throws RequestException
      */
     public function api(Request $request) {
+        // creates empty initial user input
         $message = '';
+        // checks if the user input already has been saved in the session and retrieve it
         if(Session::has('userInput')) $message = Session::get('userInput');
+        // listening for a user input from the input request , if exists replacing the message with this input
         if($request->input('query')) $message = $request->input('query');
-        $readySubmit = 'False';
-        if(Session::has('readySubmit')) $readySubmit = Session::get('readySubmit');
-        $topicFound = 'False';
-        if(Session::has('topicFound')) $topicFound = Session::get('topicFound');
-        $fileSubmit = 'False';
-        if(Session::has('fileSubmit')) $fileSubmit = Session::get('fileSubmit');
-        $file = 'None';
-        if(Session::has('file')) $file = Session::get('file');
-        $classifiedMsg = '';
 
+        // initial flag in the case when a user agreed with a topic name to search and ready to submit a file
+        $readySubmit = 'False';
+        //  checks the session for the flag 'readySubmit' and append a value to it if it's exists
+        if(Session::has('readySubmit')) $readySubmit = Session::get('readySubmit');
+
+        // initial flag to process further to a file analysis after the text classification by a chatbot
+        $topicFound = 'False';
+        //  checks the session for the flag 'topicFound' and append a value to it if it's exists
+        if(Session::has('topicFound')) $topicFound = Session::get('topicFound');
+
+        // initial flag in the case when a user agreed with a topic name to search and ready to submit a file
+        $fileSubmit = 'False';
+        //  checks the session for the flag 'fileSubmit' and append a value to it if it's exists
+        if(Session::has('fileSubmit')) $fileSubmit = Session::get('fileSubmit');
+
+        // path for the file on the server, that has been uploaded by the user for searching
+        $file = 'None';
+        // retrieve the path for the file from a session if that exists
+        if(Session::has('file')) $file = Session::get('file');
+
+        // classified message from a chatbot after the text classification step and before file analyse
+        $classifiedMsg = '';
+        //  checks the session for the classified message and append a value to it if it's exists
         if(Session::has('classifiedMsg')) $classifiedMsg = Session::get('classifiedMsg');
 
-
-
+        // initial flag in the case when a user selected a topic name after the file analysis and before web search
         $topicSelected = 'False';
+        //  checks the session for the flag 'topicSelected' and append a value to it if it's exists
+        if(Session::has('topicSelected')) $topicSelected = Session::get('topicSelected');
+
+        // stores the value for selected topic from a user to search the web
         $topicFinal = '';
+        // check the session value for selected topic and append a value to it if it's exists
         if(Session::has('topicFinal')) $topicFinal = Session::get('topicFinal');
+
+        // listening for a user input with a name 'topic' from the input request , if exists replacing the 'topicFinal' with this input  value
         if($request->input('topic')) {
             $topicFinal = $request->input('topic');
             Session::put('topicSelected','True');
         }
-        if(Session::has('topicSelected')) $topicSelected = Session::get('topicSelected');
+
 //        http://127.0.0.1:5000/
 //        https://chatbot-educ-api.herokuapp.com/
+
+        /*
+         Sends the get request to the flask api where the chatbot exists.
+         Sends the response from a chatbot back to the user
+         Stores data from the response in the session
+        */
         $arr = Http::get('http://127.0.0.1:5000/',[
                 'message'=>$message,
                 'readySubmit'=>$readySubmit,
@@ -70,6 +103,9 @@ class HomeController extends Controller {
         return response($arr, 200);
     }
 
+    /**
+     * Methof for the file analysis
+    */
     public function analyse(Request $request) {
         $file = $_POST["pdf"] ?? null;
 
--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,8 @@ Route::get('/', [HomeController::class, 'home'])->name('home');
 
 Route::get('/target-page', [HomeController::class, 'index']);
 
+Route::post('/query', [HomeController::class, 'api']);
+
 Route::post('/analyse', [HomeController::class, 'analyse']);
 
 Route::get('/topicFound', [HomeController::class, 'api']);
